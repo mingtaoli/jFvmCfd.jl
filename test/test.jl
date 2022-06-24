f75c4575154df120a8d0cc02fdaf4505abf902ac
Mingtao Li <mingtao@xjtu.edu.cn>
2022-06-24 15:58:26 +0800

    term实现方式示例

--- test/test.jl
+++ test/test.jl
@@ -1,22 +1,65 @@
 
 
-abstract type abstractScheme end
-struct divScheme <: abstractScheme end
-struct tranScheme <: abstractScheme end
-struct ADDScheme <: abstractScheme 
+abstract type abstractTerm end
+struct divTerm <: abstractTerm end
+struct tranTerm <: abstractTerm end
+struct ADDTerm <: abstractTerm 
 first
 second
 end
 
-import Base: +
+struct MinusTerm <: abstractTerm 
+    first
+    second
+end
+
+import Base: +,-
 
 
-c=divScheme()
-d=tranScheme()
 
-function (+)(a::abstractScheme, b::abstractScheme)
-    ADDScheme(a,b)
+function (+)(a::abstractTerm, b::abstractTerm)
+    ADDTerm(a,b)
     ## 应该返回一个Term
 end
 
+function (-)(a::abstractTerm, b::abstractTerm)
+    MinusTerm(a,b)
+    ## 应该返回一个Term
+end
+
+
+
+function getMatrix(divterm::divTerm)
+    [1 2 3; 4 1 6; 7 8 1]
+end
+function getMatrix(tranterm::tranTerm)
+    [5 2 5; 4 1 6; 7 8 1]
+end
+
+function getMatrix(addterm::ADDTerm)
+ getMatrix(addterm.first)+getMatrix(addterm.second)
+end
+function getMatrix(minusterm::MinusTerm)
+    getMatrix(minusterm.first)-getMatrix(minusterm.second)
+end
+
+c=divTerm()
+d=tranTerm()
+
 e=c+d
+f=c+e
+g=c+d+c
+
+g=c-d
+h=getMatrix(g)
+
+#= 
+julia> @show h
+h = [7 6 11; 12 3 18; 21 24 3]
+3×3 Matrix{Int64}:
+  7   6  11
+ 12   3  18
+ 21  24   3 =#
+
+ getMatrix(c+d)
+ getMatrix(c-d)
